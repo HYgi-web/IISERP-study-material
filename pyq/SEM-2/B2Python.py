96939a25531efb1b1680500b38a7c29bf576d50a
HYgi-web <harshyadav0107hy@gmail.com>
2024-12-29 11:19:46 +0530

    pyq

---
+++ pyq/SEM-2/B2Python.py
@@ -0,0 +1,38 @@
+
+#number=20
+#while number!=0:
+   # print (number)
+   # number=number-1
+
+
+'''for i in range(1,90,1):
+    for i in range(1,i+1):
+        print(i,end=" ")
+        print( )   
+'''
+'''#write aprogram to input number and print its square
+n= int(input("Enter any number")) 
+s=n*n
+print('sqare of the number is',s)
+'''
+
+#WAP to input four no. and find the lowest
+a=int(input("number"))
+b=int(input("number"))
+c=int(input("number"))
+d=int(input("number"))
+if(a<b):
+    x=a
+else:
+    x=b
+if(c<d):
+    y=c
+else:
+    y=d
+if(x<y):
+    l=x
+else:
+    l=y
+print('lowest number is',l)
+  
+ 
